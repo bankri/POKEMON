c5faf383c1c83d674d7332a6c5e136d5a210aa16
M. Dzikri <153479512+bankri@users.noreply.github.com>
2025-07-31 14:25:01 +0700

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,11 @@
+import PokemonList from "./components/PokemonList/PokemonList.css";
+
+function App() {
+  return (
+    <>
+      <PokemonList />
+    </>
+  );
+}
+
+export default App;
---
+++ data/pokemons.js
@@ -0,0 +1,123 @@
+export const pokemons = [
+  {
+    id: 1,
+    name: "Bulbasaur",
+    types: ["Grass", "Poison"],
+    image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png"
+  },
+  {
+    id: 2,
+    name: "Ivysaur",
+    types: ["Grass", "Poison"],
+    image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/2.png",
+    description: "When the bulb on its back grows large, it appears to lose the ability to stand on its hind legs."
+  },
+  {
+    id: 3,
+    name: "Venusaur",
+    types: ["Grass", "Poison"],
+    image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/3.png"
+  },
+  {
+    id: 4,
+    name: "Charmander",
+    types: ["Fire"],
+    image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png"
+  },
+  {
+    id: 5,
+    name: "Charmeleon",
+    types: ["Fire"],
+    image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/5.png"
+  },
+  {
+    id: 6,
+    name: "Charizard",
+    types: ["Fire", "Flying"],
+    image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/6.png"
+  },
+  {
+    id: 7,
+    name: "Squirtle",
+    types: ["Water"],
+    image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/7.png"
+  },
+  {
+    id: 8,
+    name: "Wartortle",
+    types: ["Water"],
+    image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/8.png"
+  },
+  {
+    id: 9,
+    name: "Blastoise",
+    types: ["Water"],
+    image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/9.png"
+  },
+  {
+    id: 10,
+    name: "Caterpie",
+    types: ["Bug"],
+    image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/10.png"
+  },
+  {
+    id: 11,
+    name: "Metapod",
+    types: ["Bug"],
+    image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/11.png"
+  },
+  {
+    id: 12,
+    name: "Butterfree",
+    types: ["Bug", "Flying"],
+    image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/12.png"
+  },
+  {
+    id: 13,
+    name: "Weedle",
+    types: ["Bug", "Poison"],
+    image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/13.png"
+  },
+  {
+    id: 14,
+    name: "Kakuna",
+    types: ["Bug", "Poison"],
+    image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/14.png"
+  },
+  {
+    id: 15,
+    name: "Beedrill",
+    types: ["Bug", "Poison"],
+    image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/15.png"
+  },
+  {
+    id: 16,
+    name: "Pidgey",
+    types: ["Normal", "Flying"],
+    image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/16.png"
+  },
+  {
+    id: 17,
+    name: "Pidgeotto",
+    types: ["Normal", "Flying"],
+    image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/17.png"
+  },
+  {
+    id: 18,
+    name: "Pidgeot",
+    types: ["Normal", "Flying"],
+    image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/18.png"
+  },
+  {
+    id: 19,
+    name: "Rattata",
+    types: ["Normal"],
+    image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/19.png"
+  },
+  {
+    id: 20,
+    name: "Raticate",
+    types: ["Normal"],
+    image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/20.png"
+  }
+];
